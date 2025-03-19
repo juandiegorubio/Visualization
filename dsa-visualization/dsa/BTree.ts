c514377833f6bb20c88efaa7406675c49de41018
juandiegorubio <rubio.juandiego.cs@gmail.com>
2025-03-19 21:44:30 +0100

    feat: :construction: Developing BTree structure
    TODO: balance function + when to call balance

--- dsa-visualization/dsa/BTree.ts
+++ dsa-visualization/dsa/BTree.ts
@@ -1,68 +1,135 @@
-class TreeNode {
+export class TreeNode {
 
     /*** Properties ***/
     elements: number[];
-    left_child: TreeNode | null;
-    right_child: TreeNode | null;
+    children: (TreeNode | null)[];
+    parent: TreeNode | null;
+    depth: number;
 
 
-    constructor(elements: number[] = [1,2,3,4,5]) {
-        this.elements = elements;
-        this.left_child = null;
-        this.right_child = null;
+    constructor(size: number = 2) {
+        this.elements = [];
+        this.children = [null];
+        this.parent = null; 
+        this.depth = 0;
     }
 
-    set_children(
-        left_child: TreeNode | null = null, 
-        right_child: TreeNode | null = null
-    ) {
-        this.left_child = left_child;
-        this.right_child = right_child;
+    get_child(
+        pos: number
+    ) : (TreeNode | null) {
+        return this.children[pos];
     }
 
-    find_position(element: number) : number {
+    find_interval(
+        element: number
+    ) : number {
         let i: number = 0;
         while( i < this.elements.length && element > this.elements[i] ) i++;
         return i;
     }
 
-    find(element: number) : number {
-        const pos : number = this.find_position(element);
-        if( element === this.elements[pos] ) return pos;
-        else return -1;
+    find(
+        element: number
+    ) : number {
+        const pos : number = this.find_interval(element);
+        if(pos == this.elements.length || element != this.elements[pos]) return -1;
+        else return pos;
     }
 
+    push(
+        element: number
+    ) : number {
+        const pos : number = this.find_interval(element);
+        this.elements.splice(pos, 0, element);
+        return pos;
+    }
+
+    length() : number {
+        return this.elements.length;
+    }
 
 };
 
 
 
-class Tree {
+export default class Tree {
     
     /*** Properties ***/
     root: TreeNode;
-    max_node_size: number;
-    tree_depth: number;
+    max_size: number;
 
 
     constructor(max_size: number) {
         this.root = new TreeNode();
-        this.max_node_size = max_size;
-        this.tree_depth = 1;
+        this.max_size = max_size;
     }
 
-    _find_leaf(element: number, node: TreeNode) {
-        const pos : number = node.find()
-
+    find_leaf(
+        element: number, 
+        node: TreeNode = this.root
+    ) : TreeNode {
+        const pos : number = node.find_interval(element);
+        const child : (TreeNode | null) = this.root.get_child(pos);
+        if(child == null) return node;
+        else return this.find_leaf(element, child);
     }
 
-    find_leaf(element: number) {
 
+    insert(element: number) : ({
+        leaf: TreeNode, 
+        position: number
+    }) {
+        let leaf : TreeNode = this.find_leaf(element);
+        const pos : number = leaf.push(element);
+        if(leaf.length() > this.max_size) leaf = this.split(leaf);
+        return {leaf, position: pos};
+    }
+
+    split(leaf: TreeNode) : TreeNode {
+        const middle : number = Math.floor(leaf.length() / 2);
+
+        // Left leaf
+        const left_child : TreeNode = new TreeNode();
+        for(let i=0; i < middle; ++i) left_child.push(leaf.elements[i]);
+        left_child.parent = leaf;
+        left_child.depth = leaf.depth+1;
+        leaf.children.push(left_child);
+        
+        // Right leaf
+        const right_child : TreeNode = new TreeNode();
+        for(let i=middle+1; i < leaf.length(); ++i) right_child.push(leaf.elements[i]);
+        right_child.parent = leaf;
+        right_child.depth = leaf.depth+1;
+        leaf.children.push(right_child);
+
+        // TODO: balance if deepest leaf is way deeper than expected
+        return leaf;
+    }
+
+    // TODO: related to balance in split function
+    find_min_depth() : number {
+        return 0;
     }
 
 
-    insert(element: number) {
+    balance(node: TreeNode) {
+        let balance_root : TreeNode = node;
+        while(balance_root.parent != null && balance_root.parent.length() == this.max_size) 
+            balance_root = balance_root.parent;
+        
+        const tree_elements : number[] = this.retrieve_elements(balance_root);
 
+
+        console.log("BTree balanced!");
     }
 
+
+    retrieve_elements(node: TreeNode) : number[] {
+        let list : number[] = [];
+        for(let i = 0; i < node.length(); ++i) {
+            if(node.children[i] != null)
+                list = list.concat(this.retrieve_elements(node.children[i]!))
+        }
+        return [];
+    }
 };
